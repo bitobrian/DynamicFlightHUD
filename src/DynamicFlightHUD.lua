64afcafa7b8545bbf0d432c13bc7f5a4d3de6815
Brian Donaldson <nairbsd@gmail.com>
2023-11-28 21:25:02 -0600

    Working detect dragonriding when mounted

--- src/DynamicFlightHUD.lua
+++ src/DynamicFlightHUD.lua
@@ -1,13 +1,120 @@
-local isMountedBro = false
+local isDevelopment = true
+local maxBuffCount = 20
+local dynamicFlightMountDb = {}
+local isDbLoaded = false
+local isMounted = false
 
 function InitializeAddon(self)
     self:RegisterEvent("ADDON_LOADED")
-    self:RegisterEvent("PLAYER_MOUNT_DISPLAY_CHANGED") -- Fired when the player mounts or dismounts
 end
 
 function OnEventReceived(self, event, ...)
-    print(isMountedBro)
+    if ( event == "ADDON_LOADED" ) then
+        local arg1 = ...
+        if ( arg1 == "DynamicFlightHUD" ) then
+            Log(arg1)
+            self:UnregisterEvent("ADDON_LOADED")
+            self:RegisterEvent("PLAYER_MOUNT_DISPLAY_CHANGED")
+
+            -- Initialize the database
+            if not dynamicFlightMountDb then
+                dynamicFlightMountDb = {}
+            end
+
+            GetDynamicFlightMounts()
+        end
+    end
+end
+
+function OnMountEventReceived(self, event, ...)
     if ( event == "PLAYER_MOUNT_DISPLAY_CHANGED" ) then
         print("PLAYER_MOUNT_DISPLAY_CHANGED")
-	end
+        isMounted = true
+        IsDragonridingMount()
+    end
+end
+
+-- Functions
+
+function GetDynamicFlightMounts()
+    local mountIds = C_MountJournal.GetMountIDs()
+    -- Log(mountIds)
+
+    -- log the count of mountIds
+    if isDevelopment then
+        Log("======== MOUNT IDS ============")
+        Log(#mountIds)
+    end
+
+    -- check if mountIds is empty or nil
+    if not mountIds then
+        Log("No mounts found")
+    end
+
+    local count = 0
+
+    for i = 1, #mountIds do
+        local _, _, _, _, _, _, _, _, _, _, _, _, isForDragonriding = C_MountJournal.GetMountInfoByID(mountIds[i])
+
+        if isForDragonriding then
+            count = count + 1
+            local mountName = C_MountJournal.GetMountInfoByID(mountIds[i])
+            dynamicFlightMountDb[mountName] = true
+
+            if isDevelopment then
+                Log("======== DRAGONRIDING MOUNTS ============")
+                Log(mountName)
+            end
+        end
+    end
+
+    Log("======== DRAGONRIDING MOUNTS ============")
+    Log(count)
+
+    isDbLoaded = true
+end
+
+function IsDragonridingMount()
+    local currentMount = GetCurrentMountedMount()
+    Log(currentMount)
+    if currentMount then
+        print("Currently mounted on:", currentMount)
+    else
+        print("Not a dynamic flying mount.")
+    end
+end
+
+function GetCurrentMountedMount()
+    local playerBuffs = {}
+
+    -- Collect all buffs on the player
+    for i = 1, maxBuffCount do
+        local name, _, _, _, _, _, _, _, _, spellID = UnitBuff("player", i)
+        if name then
+            playerBuffs[spellID] = name
+        end
+    end
+
+    -- print the buffs
+    if isDevelopment then
+        Log("======== PLAYER BUFFS ============")
+        for k, v in pairs(playerBuffs) do
+            print(k, v)
+        end
+    end
+
+    -- Check if any of the buffs match our mount database
+    for k, v in pairs(playerBuffs) do
+        if dynamicFlightMountDb[v] then
+            return v
+        end
+    end
+
+    return nil -- No mount found
+end
+
+function Log(var)
+    if isDevelopment then
+        print(var)
+    end
 end
